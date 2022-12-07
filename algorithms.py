d80697c8962ed1a1744b787edb38904edfb1b4ac
aerdelez <andro.erdelez@gmail.com>
2022-12-07 18:44:21 +0100

    everything implemented, proper plotting left

--- algorithms.py
+++ algorithms.py
@@ -1,25 +1,62 @@
+import matplotlib
 import numpy as np
 import objects
 import random
+import matplotlib.pyplot as plt
 
 
-def run_algorithm(bandit, N, algoName):
-    totalRewardAction = np.zeros(len(bandit.get_actions()))  # the amount of reward for each action
-    totalActionInstances = np.zeros(len(bandit.get_actions()))  # amount of times each action has been selected
-    averageExpectedReward = np.ones(len(bandit.get_actions()))  # average reward per action, initialized to 1
-    action_preferences = np.ones(len(bandit.get_actions())) # action preferences per action, initialized to 1
-    for iteration in range(0, N):
-        action = algo_chooser(algoName, averageExpectedReward)  # select an action according to the algorithm
-        reward = bandit.execute_actions(action)
-        # update the total reward for the action, the amount of action instances, and the average expected reward for it
-        totalRewardAction[action] += reward
-        totalActionInstances[action] += 1
-        averageExpectedReward[action] = float(totalRewardAction[action]) / float(totalActionInstances[action])
+def plot_graph(listOfLists, title = "new2"):
+    label_set = ["egreedy_gaussian", "egreedy_bernoulli", "greedy_gaussian", "greedy_bernoulli", "optimistic_gauss",
+                 "optimistic_bernoulli", "ucb_gaussian", "ucb_bernoulli", "action_pref_gauss", "action_pref_bernoulli"]
+    N = len(listOfLists[0])
+    zeros = [0] * N
+    for index in range(len(listOfLists)):
+        plt.plot(listOfLists[index], label = label_set[index])
+    plt.legend(label_set)
+    plt.title(title)
+    plt.ylabel("Average reward")
+    plt.xlabel("Iteration")
+    plt.show()
 
-    # calculate the total reward
+
+def run_algorithm(bandit, N, algoName, ucb, optimistic, preferences):
+    totalRewardAction = np.zeros(len(bandit.get_actions()))      # the amount of reward for each action
+    totalActionInstances = np.zeros(len(bandit.get_actions()))   # amount of times each action has been selected
+    averageExpectedReward = np.zeros(len(bandit.get_actions()))  # average reward per action, initialized to 0
+    probabilities = np.zeros(len(bandit.get_actions()))
+    if optimistic:
+        averageExpectedReward = np.ones(len(bandit.get_actions()))  # average reward per action, initialized to 1
+    previous_action = -1
+
     totalReward = 0
-    for reward in totalRewardAction:
+    rewardList = []
+    for iteration in range(0, N):
+        # select an action according to the algorithm
+        if preferences:
+            if iteration == 0:
+                action = random.choice(list(range(0, len(probabilities))))
+            else:
+                action = random.choices(list(range(0, len(probabilities))), weights=probabilities, k=1)[0]
+        else:
+            action = algo_chooser(algoName, averageExpectedReward)
+        reward = bandit.execute_actions(action)
         totalReward += reward
+        rewardList.append(totalReward / float(iteration + 1))
+        # update the total reward for the action, the amount of action instances, and the average expected reward for it
+        totalRewardAction[action] += float(reward)
+        totalActionInstances[action] += 1
+        # implementation of action preferences
+        if preferences:
+            averageExpectedReward = update_preferences(previous_action, action,
+                                                       averageExpectedReward, 1/float(totalActionInstances[action]),
+                                                       reward, averageExpectedReward[action])
+            probabilities = update_probabilities(averageExpectedReward, probabilities)
+        # implementation of upper confidence-bound
+        elif ucb:
+            averageExpectedReward = u_c_b(averageExpectedReward, totalRewardAction, totalActionInstances, iteration)
+        else:
+            averageExpectedReward[action] = float(totalRewardAction[action]) / float(totalActionInstances[action])
+        previous_action = action
 
     # the amount of correct answers is the index of the answer with the highest reward's frequency
     best_answer = bandit.get_current_best_action()
@@ -27,7 +64,14 @@ def run_algorithm(bandit, N, algoName):
     # return the total reward, and the total no. of optimal choices
     if totalReward >= 1000:
         print(averageExpectedReward)
-    return totalReward / float(N), totalCorrect / float(N)
+    return totalReward / float(N), totalCorrect / float(N), rewardList
+
+
+def update_probabilities(preferences, probabilities):
+    for i in range(len(preferences)):
+        probabilities[i] = boltzmann_distribution(i, preferences)
+
+    return probabilities
 
 
 def algo_chooser(algoName, average_expected_reward):
@@ -35,10 +79,6 @@ def algo_chooser(algoName, average_expected_reward):
         return greedy(average_expected_reward)
     elif algoName == "e_greedy":
         return e_greedy(average_expected_reward)
-    elif algoName == "optimistic":
-        return optimistic_initial_values(len(average_expected_reward))
-    elif algoName == "ucb":
-        return
 
 
 def greedy(average_expected_reward):
@@ -53,41 +93,38 @@ def e_greedy(average_expected_reward, epsilon=0.1):
         return np.argmax(average_expected_reward)
 
 
-def optimistic_initial_values(length):
-    optimistic_estimates = []
-    for i in range(length):
-        optimistic_estimates.append(random.randrange(70, 100))
+def u_c_b(average_expected_reward, total_action_reward, total_action_instances, t):
+    for i in range(len(average_expected_reward)):
+        if total_action_instances[i] == 0:
+            average_expected_reward[i] = calculate_uncertainty(total_action_instances[i], t + 1)
+        else:
+            average_expected_reward[i] = float(total_action_reward[i]) / float(total_action_instances[i]) \
+                                    + calculate_uncertainty(total_action_instances[i], t + 1)
 
-    return optimistic_estimates
-
-
-def ucb(actions, estimates, action_counters, t):
-    ucb_estimates = []
-    for i in range(len(actions)):
-        ucb_estimates.append(estimates[i] + calculate_uncertainty(action_counters[i], t))
-    return greedy(actions)
+    return average_expected_reward
 
 
 def calculate_uncertainty(n, t):
-    c = 1.5  # value with which we tamper for better performance
+    c = 5.5  # value with wich we tamper for better performance
+    if n == 0:
+        return c * np.sqrt(np.log(t))
     return c * np.sqrt(np.log(t) / n)
-
-
-def action_preferences():
-    return
 
 
 def update_preferences(previous_action_idx, chosen_action_idx, preferences, alpha, reward, average_reward):
     policy = boltzmann_distribution(chosen_action_idx, preferences)
     preferences[chosen_action_idx] = preferences[chosen_action_idx] + alpha * (reward - average_reward) * (1 - policy)
-    if previous_action_idx != chosen_action_idx:
+    if previous_action_idx != chosen_action_idx and previous_action_idx != -1:
         preferences[previous_action_idx] = preferences[chosen_action_idx] + alpha * (reward - average_reward) * policy
 
     return preferences
 
 
 def boltzmann_distribution(idx, preferences):
-    return np.exp(preferences[idx]) / np.sum(preferences)
+    sum = 0
+    for x in preferences:
+        sum += np.exp(x)
+    return np.exp(preferences[idx]) / sum
 
 
 def make_bandit(actionCount, bernoulli):
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import algorithms
 
 if __name__ == "__main__":
-    N, actionCount, experimentCount = 1000, 50, 1000
-    for algo in range(0, 2):
+    N, actionCount, experimentCount = 1000, 50, 50
+    optimistic, ucb, preferences = 0, 0, 0
+    print("Experiments run with " + str(N) + " iterations, " +
+          str(actionCount) + " actions and " + str(experimentCount) + " experiments per setup.")
+    rewardsPerAlgo = []
+    for algo in range(5):
         if algo == 0:                  # set the algorithm that we want to use
-            algorithm = "greedy"
-        elif algo == 1:
             algorithm = "e_greedy"
+        elif algo == 1:
+            algorithm = "greedy"
+        elif algo == 2:
+            optimistic = 1
+        elif algo == 3:
+            optimistic = 0
+            ucb = 1
+        elif algo == 4:
+            ucb = 0
+            preferences = 1
 
         for bernoulli in range(0, 2):  # run the gaussian and bernoulli version of the problem
             greedyAverage, greedyCorrect = 0, 0
+            rewardList = [0] * N
             for experiment in range(0, experimentCount):
                 BanditProblem = algorithms.make_bandit(actionCount, bernoulli)
-                average, correct = algorithms.run_algorithm(BanditProblem, N, algorithm)
+                average, correct, experimentList = algorithms.run_algorithm(BanditProblem, N, algorithm, ucb, optimistic, preferences)
+                for index in range(len(experimentList)):
+                    rewardList[index] += experimentList[index]
                 greedyAverage += average
                 greedyCorrect += correct
 
@@ -20,7 +35,22 @@ if __name__ == "__main__":
                 print("Average reward and correct percentage for " + algorithm + " in the bernoulli problem:")
             else:
                 print("Average reward and correct percentage for " + algorithm + " in the gaussian problem:")
+            if ucb:
+                print("Using ucb")
+            if optimistic:
+                print("Using optimistic initial values")
+            if preferences:
+                print("Using action preferences")
             greedyAverage /= float(experimentCount)  # average reward obtained in a run of the algorithm
             greedyCorrect /= float(experimentCount)  # average amount of times the algorithm selects the best option
             print("Average reward: " + str(greedyAverage))
             print("Proportion of correct answers: " + str(greedyCorrect))
+
+            print()
+
+            for index in range(len(rewardList)):
+                rewardList[index] /= experimentCount
+            rewardsPerAlgo.append(rewardList)
+
+    # plot the things
+    algorithms.plot_graph(rewardsPerAlgo)
